25cd1a2469475cdb1d835632ad602416054a006c
Robin Marx <marx.robin@gmail.com>
2021-12-04 15:05:10 +0100

    Day 4

---
+++ src/template.ts
@@ -0,0 +1,22 @@
+
+{
+    function execute(input:String) {
+    
+        return execute1(input);
+        // return execute2(input);
+    }
+    
+    function execute1(input:String):void {
+ 
+    }
+    
+    function execute2(input:String):void {
+    
+    }
+    
+    
+    
+    const input = ``;
+    
+    execute(input);
+}
