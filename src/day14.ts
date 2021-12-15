ef5864fba79a66cc18cb265cdcec324b03d87a97
Robin Marx <robin.marx@kuleuven.be>
2021-12-15 17:54:57 +0100

    Day 14 cleanup

--- src/day14.ts
+++ src/day14.ts
@@ -84,9 +84,6 @@
     
     function execute2(polymer:string[], substitutions:Map<string, string>):void {
 
-        // well... now we're f*cked... need to use memoization again to be able to calculate this
-        // however, things are more difficult than for day 6, since we have overlapping characters at the edges that we shouldn't count twice...
-
         interface ResultCounts {
             results:Map<string, number> // for a given character (e.g., C), stores how many of that character were present in the output
         }
@@ -97,16 +94,13 @@
 
         // will calculate a number of substitutions of the start string
         // the idea is that you pass a single pair (e.g., CN, FF) and it will calculate the polymer outcome for that pair inputs
-        const iteratePolymer = function( polymer:string[], substitutions:Map<string, string>, iterations:number ):string[] {
+        const extendPolymer = function( polymer:string[], substitutions:Map<string, string>, iterations:number ):string[] {
 
             const polymerKey = polymer.join("") + "@" + iterations;
 
-            // this is probably not needed
             if ( polymerCache.has(polymerKey) ) {
                 return polymerCache.get(polymerKey)!;
             }
-            
-            console.log("iteratePolymer", polymer);
 
             for ( let step = 0; step < iterations; ++step ) {
 
@@ -115,7 +109,6 @@
                     const substitution = substitutions.get( pair );
     
                     if ( substitution !== undefined ) {
-    
                         polymer.splice( charIndex + 1, 0, substitution );
                         charIndex += 1; // skip the newly inserted one manually
                     }
@@ -149,94 +142,58 @@
 
         const getScoreForPolymer = function( polymer:string[], step:number ):ResultCounts {
 
-            // given a polymer, we want to extend it 40 steps and calculate the occurrences of each letter
-            // doing 40 steps at once is too slow, so we do this recursively in steps of 10
-            // - extend polymer 10 steps
-            //      - take the first pair of letters of the extended polymer and extend those 10 steps
-            //      - take the first pair of this new extension and extend 10 steps again
-            //      - do this again one final time
-            //           - we can now calculate the letter occurrences for a small part of the final polymer: store those, we can discard the extended polymer
-            //      - now take the next letter-pair of the 30-deep extension, extend 10 steps, and calculate occurrences
-            //      - keep doing this until the first 30-deep extension is done. Accumulate occurrences across them and store that
-            //          - return from the recursion and go for the next 2-letter pair at 20-deep etc. 
-            //      -> We make this faster by caching both the polymer-extensions at 10-deep, as well as scores at 40, 30, 20 and 10 deep
-
-
             // console.log(step + "/" + bigSteps + ":getScoreForPolymer", polymer );
 
             const polymerKey = polymer.join("") + "@" + step; 
 
+            // we build a countCache entry for each step, not just the final step
+            // so check it for each invocation
             if ( countCache.has( polymerKey ) ) {
                 return countCache.get( polymerKey )!;
             }
 
-            if ( step == bigSteps + 1 ) { // step 5
-                const individualCharacters = new Set( polymer );
+            if ( step == bigSteps + 1 ) { // final depth, calculate actual occurrences
 
                 const output:ResultCounts = { results: new Map<string, number>() };
 
+                const individualCharacters = new Set( polymer );
                 for( let char of individualCharacters ) {
-                    // this is PROBABLY not the most performant... but we'll see
                     output.results.set( char, polymer.filter( (pchar) => pchar === char ).length );
                 }
 
                 countCache.set( polymerKey, output );
 
-                // console.log("CHARACTERS FROM POLYMER", polymer, individualCharacters, output);
-
                 return output;
             }
             else {
+                // ASSUMPTION: The polymer we're passed in is always of length 2!
+                const extendedPolymer = extendPolymer( polymer, substitutions, cacheLineSize );
 
-                // step 1: will extend to 10 steps and call recursion for step 2
-                // step 2: will extend to 20 steps and call recursion for step 3
-                // step 3: will extend to 30 steps and call recursion for step 4
-                // step 4: will extend to 40 steps and call recursion for step 5
-                //      step 5: this is where we calculate the actual scores for the final extended polymer, see above
-
-                let output:ResultCounts = { results: new Map<string, number>() };
-
-                for( let charIndex = 0; charIndex < polymer.length - 1; ++charIndex ) { // TODO: this shouldn't be needed besides the top-level call, as for the rest we only call this function with polymers of length 2
-                    const pair = "" + polymer[charIndex] + polymer[charIndex + 1];
-                
-                    const extendedPolymer = iteratePolymer( [ polymer[charIndex], polymer[charIndex + 1] ], substitutions, cacheLineSize );
-
-                    // console.log( pair + " extended into " + extendedPolymer );
-                    
-                    for( let extendedCharIndex = 0; extendedCharIndex < extendedPolymer.length - 1; ++extendedCharIndex ) {
-                        // console.log( output );
-                        mergeCounts( output, getScoreForPolymer( [extendedPolymer[extendedCharIndex], extendedPolymer[extendedCharIndex + 1]] , step + 1) );
-                    }
-
-                    // we've now counted some letter double, because they are re-used in the polymer extensions
-                    // so we need to remove the doubles for everything that's not either start or end letter
-                    for ( let c = 1; c < extendedPolymer.length - 1; ++c ) {
-                        output.results.set( extendedPolymer[c], output.results.get( extendedPolymer[c] )! - 1 );
-                    }
+                let occurrences:ResultCounts = { results: new Map<string, number>() };
+                for( let charIndex = 0; charIndex < extendedPolymer.length - 1; ++charIndex ) {
+                    mergeCounts( occurrences, getScoreForPolymer( [extendedPolymer[charIndex], extendedPolymer[charIndex + 1]] , step + 1) );
                 }
 
-                // console.log("SET COUNTCACHE", polymerKey, output);
-                countCache.set( polymerKey, output );
+                // we've now counted some letters double, because they are re-used in the polymer extensions
+                // so we need to remove the doubles for everything that's not either start or end letter
+                for ( let c = 1; c < extendedPolymer.length - 1; ++c ) {
+                    occurrences.results.set( extendedPolymer[c], occurrences.results.get( extendedPolymer[c] )! - 1 );
+                }
 
-                return output;
+                countCache.set( polymerKey, occurrences );
+
+                return occurrences;
             }
 
         }
 
-        const results:ResultCounts = getScoreForPolymer( polymer, 1 ); // step 1 = 10, 2 = 20, 3 = 30 and 4 = 40
+        // getScoreForPolymer expects to get a single pair of letters as polymer, so split the input here
+        const results:ResultCounts = { results: new Map<string, number>() };
+        for( let charIndex = 0; charIndex < polymer.length - 1; ++charIndex ) {
+            mergeCounts( results, getScoreForPolymer( [ polymer[charIndex], polymer[charIndex + 1]], 1 ));
+        }
 
-        console.log( results );
-
-        // need to handle duplicates due to overlapping starting pairs
-        // in the end, it turns out that with our method, we'll just have some off-by-ones for the characters in the middle of the input polymer string
-        // NNCB
-        // After step 10, 
-        // B occurs 1749 times,   1749     // exact, even though in the initial polymer (is at the back though, isn't taken later)
-        // C occurs 298 times,     299      // off by one, once in initial
-        // H occurs 161 times, and  161    // exact
-        // N occurs 865 times       866    // off by one, even though it's in the initial twice (it's at the start though)
-        // so let's just filter the unnecessary ones out
-
+        // need to remove double counted chars
         for ( let charIndex = 1; charIndex < polymer.length - 1; ++charIndex ) {
             if ( results.results.has( polymer[charIndex] ) ) {
                 results.results.set( polymer[charIndex], results.results.get( polymer[charIndex] )! - 1 );
@@ -247,19 +204,6 @@
         let minCount = Number.MAX_SAFE_INTEGER;
 
         console.log( results );
-        /* 
-            should be:
-            'P' => 2123534739569,
-            'K' => 1873105930252,
-            'C' => 2646198121264,
-            'B' => 1574027242624,
-            'F' => 3423703277376,
-            'N' => 1872052600109,
-            'S' => 1772658891477,
-            'H' => 4290780551024,
-            'V' => 840276158486,
-            'O' => 474383415564
-        */
 
         for( let char of results.results.keys() ) {
             const count = results.results.get(char)!;
@@ -272,7 +216,7 @@
             }
         }
 
-        console.log( "Final score:", maxCount - minCount ); // should be 3816397135460
+        console.log( "Final score:", maxCount - minCount, (maxCount - minCount) === 3816397135460  ); // should be 3816397135460
     }
     
     
